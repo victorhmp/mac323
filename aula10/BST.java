0438e133cbf598561745fbd842d137338a7f5fda
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-17 11:47:29 -0300

    Add methods for deleting the minimum and deleting a certain element

--- aula10/BST.java
+++ aula10/BST.java
@@ -76,4 +76,33 @@ public class BST<Key extends Comparable<Key>, Value>{
     x.left = deleteMin(x.left);
     return x;
   }
+
+  public void delete(Key key) {
+    r = delete(r, key);
+  }
+  private Node delete(Node x, Key key) {
+    if (x == null) return null;
+
+    int cmp = key.compareTo(x.key);
+
+    if (cmp < 0) {
+      x.left = delete(x.left, key);
+    }
+    else if (cmp > 0) {
+      x.right = delete(x.right, key);
+    }
+    else {
+      if (x.right == null) {
+        return x.left;
+      }
+      else if (x.left == null) {
+        return x.right;
+      }
+      Node t = x;
+      x = min(t.right);
+      x.right = deleteMin(t.right);
+      x.left = t.left;
+    }
+    return x;
+  }
 }
