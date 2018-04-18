2c15fa408225662afd9710546bcf4cc4d74737fb
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-04-17 22:43:38 -0300

    Add API specification
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ EP03/Solver.java
@@ -0,0 +1,21 @@
+public class Solver {
+    // Constructor that also finds the solution
+    public Solver(Board initial) {
+
+    }
+
+    // Returns the number of moves to solve initial puzzle
+    public int moves () {
+
+    }
+
+    // Returns sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+
+    }
+
+    // Test client
+    public static void main(String[] args) {
+
+    }
+}
