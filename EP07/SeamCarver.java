da4a18a1a2fde02db294dcea67377eef91983f6d
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-07-07 16:45:00 -0300

    Implement `findHorizontalSeam()` and transposePicture() methods
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- EP07/SeamCarver.java
+++ EP07/SeamCarver.java
@@ -46,7 +46,7 @@ public class SeamCarver {
         return getPicHeight();
     }
 
-    private double getPixelEnergy(int x, int y) {
+    private double getPixelEnergy(int x, int y, Color[][] colors) {
         Color left, right, up, down;
 
         if (x == 0) {
@@ -92,18 +92,86 @@ public class SeamCarver {
             throw new IllegalArgumentException();
         if (y < 0 || y > this.picHeight-1)
             throw new IllegalArgumentException();
-        return getPixelEnergy(x, y);
+        return getPixelEnergy(x, y, this.colors);
     }
 
-    // public int[] findHorizontalSeam() {
-    //     // sequence of indices for horizontal seam
-    // }
+    private double[][] transposePicture() {
+        double transposeEnergy[][] = new double[this.picWidth][this.picHeight];
+        double originalEnergy[][] = createEnergyMatrix();
+
+        for (int i = 0; i < this.picHeight; i++) {
+            for (int j = 0; j < this.picWidth; j++) {
+                transposeEnergy[j][i] = originalEnergy[i][j];
+            }
+        }
+
+        return transposeEnergy;
+    }
+    private int[] findHorizontalSeam(double[][] pixelEnergy) {
+        int[] pixelTo = new int[this.picWidth * this.picHeight];
+        double[] energyTo = new double[this.picWidth * this.picHeight];
+        
+        for (int x = 1; x < this.picWidth - 1; x++) {
+            for (int y = 1; y < this.picHeight - 1; y++) {
+                energyTo[x * this.picHeight + y] = Double.POSITIVE_INFINITY;
+            }
+        }
+
+        for (int x = 1; x < this.picWidth - 1; x++) {
+            for (int y = 1; y < this.picHeight - 1; y++) {
+                int prevPix = (x - 1) * this.picHeight + y;
+                int leftPix = x * this.picHeight + y - 1;
+                int midPix = x * this.picHeight + y;
+                int rightPix = x * this.picHeight + y + 1;
+                
+                double energyLeft = pixelEnergy[x][y - 1] + energyTo[prevPix];
+                double energyMid = pixelEnergy[x][y] + energyTo[prevPix];
+                double energyRight = pixelEnergy[x][y + 1] + energyTo[prevPix];
+                
+                if (leftPix != 0 && energyTo[leftPix] > energyLeft) {
+                    energyTo[leftPix] = energyLeft;
+                    pixelTo[leftPix] = y;
+                }
+                
+                if (energyTo[midPix] > energyMid) {
+                    energyTo[midPix] = energyMid;
+                    pixelTo[midPix] = y;
+                }
+                
+                if (rightPix != this.picHeight - 1 && energyTo[rightPix] > energyRight) {
+                    energyTo[rightPix] = energyRight;
+                    pixelTo[rightPix] = y;
+                }
+            }
+        }
+
+        int[] horizontalSeam = new int[this.picWidth];
+        double lowestEnergy = Double.POSITIVE_INFINITY;
+
+        for (int y = 1; y < this.picHeight - 1; y++) {
+            int curPix = (this.picWidth - 2) * this.picHeight + y;
+            if (energyTo[curPix] < lowestEnergy) {
+                lowestEnergy = energyTo[curPix];
+                horizontalSeam[this.picWidth - 2] = y;
+                horizontalSeam[this.picWidth - 1] = y;
+            }
+        }
+
+        for (int x = this.picWidth - 3; x >= 0; x--){
+            horizontalSeam[x] = pixelTo[(x + 1) * this.picHeight + horizontalSeam[x + 1]];
+        }
+
+        return horizontalSeam;       
+    }
+    public int[] findHorizontalSeam() {
+        return findHorizontalSeam(transposePicture());
+    }
     
     private double[][] createEnergyMatrix() {
         double[][] pixelEnergy = new double[this.picHeight][this.picWidth];
         for (int i = 0; i < this.picHeight; i++) {
             for (int j = 0; j < this.picWidth; j++) {
-                pixelEnergy[i][j] = energy(i, j);
+                pixelEnergy[i][j] = energy(j, i);
             }
         }
 
